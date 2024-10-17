faaea5d3b08abe48488b9b24844b24377f07de07
AnaCGonzalez <ana@atomboxcrm.com>
2024-10-16 18:17:21 -0700

    Add files via upload

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// Import plugins Tab
+require('cypress-plugin-tab')
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
